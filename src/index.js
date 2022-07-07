8ae11dc35e72740fb0a6a50b88f797232b6c02cf
the-dmitry <dzmitrygaponenko@gmail.com>
2022-07-07 18:52:29 +0300

    refactor: add solution through recursion

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 module.exports = function toReadable (number) {
-    let arr = number.toString().split('').map(n => +n)
-    let result = ''
+    let stringNum = number.toString()
+    let arr = [stringNum.slice(0, 1), stringNum.slice(1)]
+    let result = []
     let toTwenty = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 
     'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
     let tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-  
-    if(number == 0) {
-        result = 'zero' 
-    } else if (number < 20) { 
-      result = `${toTwenty[number]}`
-    } else if (number > 19 && number < 100) {
-      result = `${tens[arr[0]]} ${toTwenty[arr[1]]}`
-    } else if (number > 99) {
-      arr[1] == 1 ? result = `${toTwenty[arr[0]]} hundred ${toTwenty[arr[2]+10]}` : result = `${toTwenty[arr[0]]} hundred ${arr[1] == 0 ? '' : tens[arr[1]]+ ' '}${toTwenty[arr[2]]}`
+
+    if(number === 0 && number.toString().length == 1) {
+        return 'zero'
+    } else if(number < 20) {
+        return toTwenty[number]
+    } else if(number > 19 && number < 100) {
+        result.push(tens[+arr[0]])
+        result.push(toTwenty[+arr[1]])
+    } else {
+        result.push(toTwenty[+arr[0]] + ' hundred');
+        result.push(toReadable(+arr[1]));
     }
-  
-    return result.trim();
+    result[result.length - 1] == 'zero' ? result.pop() : 'zero'
+    return result.join(' ').trim()
 }
